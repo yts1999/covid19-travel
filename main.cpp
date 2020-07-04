295758827544ef4494947896e765021b45e98d24
yts1999 <yts1999@yeah.net>
2020-07-05 01:31:42 +0800

    add logger

--- main.cpp
+++ main.cpp
@@ -1,10 +1,46 @@
 #include "mainwindow.h"
 #include "args.h"
 #include <QApplication>
+#include <QDateTime>
+#include <QFile>
+#include <QTextStream>
 
-int main(int argc, char *argv[])
-{
+void outputMessage(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+    QByteArray localMsg = msg.toLocal8Bit();
+
+    QString strType = "";
+    switch (type) {
+        case QtDebugMsg:
+            strType = "Debug";
+            break;
+        case QtWarningMsg:
+            strType = "Warning";
+            break;
+        case QtCriticalMsg:
+            strType = "Critical";
+            break;
+        case QtFatalMsg:
+            strType = "Fatal";
+            break;
+        case QtInfoMsg:
+            strType = "Info";
+            break;
+    }
+
+    QString strDateTime = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss");
+    QString strMessage = QString("[%1][%2]%3").arg(strType).arg(strDateTime).arg(msg);
+
+    QFile file("log.txt");
+    file.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream stream(&file);
+    stream << strMessage << "\n";
+    stream.flush();
+    file.close();
+}
+
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    qInstallMessageHandler(outputMessage);
     MainWindow w;
     w.show();
     return a.exec();
